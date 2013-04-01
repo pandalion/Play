76b817e30d43673091b2b4cd29e89a3e0a105b7b
Paula Norris <thepandalion@gmail.com>
2013-04-01 22:22:05 +0100

    Super simple console text adventure based on codecademy tutorial

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,23 @@
 // Teh Javascripts!
+$(window).bind("load", function() {
+    var userAge = prompt("How old are you?");
+    var intro;
+
+    if (userAge < 18) {
+        intro = "Once upon a time there was a very cuddly panda. He liked to laze around and eat bamboo."
+    } else {
+        intro = "Once upon a time there was a very angry panda. He was very big with sharp claws."
+    }
+
+    console.log(intro);
+
+    console.log("Panda stares at you");
+
+    var userAnswer = prompt("Do you want to hug panda?");
+
+    if (userAnswer === "yes") {
+        console.log("Panda liked the hug!");
+    } else {
+        console.log("Panda looks sad");
+    }
+});
